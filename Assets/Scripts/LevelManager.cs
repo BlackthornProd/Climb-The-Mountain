a3b1be8c45bb1aa6492d49385cf5bb55632b6c79
Liam-Calice <liamvab@gmail.com>
2017-10-22 12:17:13 +0200

    Player Controller & Level Generator

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+
+	public GameObject[] rooms;
+	public Vector2 numberRooms;
+
+	[Space]
+
+	public GameObject[] specialTiles;
+	public Vector2 numberOfSpecialTilesPerRoomMinMax;
+
+		
+	void Start () {
+		for (int y = 0; y < numberRooms.y; y++) {
+			for (int x = 0; x < numberRooms.x; x++) {
+				SpawnRoom (y, x);
+			}
+		}
+	}
+		
+	void SpawnRoom (int y, int x) {
+		GameObject newRoom = (GameObject)Instantiate (rooms[Random.Range (0, rooms.Length)], new Vector2 (x * 10, y * 10), Quaternion.identity);
+		newRoom.name = "Room" + y + "/" + x;
+		int numberOfSpecialTilesInRoom = (int)Random.Range (numberOfSpecialTilesPerRoomMinMax.x, numberOfSpecialTilesPerRoomMinMax.y + 1);
+		for (int i = 0; i < numberOfSpecialTilesInRoom; i++) {
+			Transform platforms = newRoom.transform.Find ("Platforms");
+			int numberOfTilesInRoom = platforms.childCount;
+			int randomTileInRoomIndex = Random.Range (0, numberOfTilesInRoom);
+			Transform tileToChangeTransform = platforms.GetChild (randomTileInRoomIndex);
+			GameObject tileToChangeGO = tileToChangeTransform.gameObject;
+			Destroy (tileToChangeGO);
+			GameObject newSpecialTile = (GameObject)Instantiate (specialTiles[Random.Range (0, specialTiles.Length)], tileToChangeTransform.position, Quaternion.identity);
+			newSpecialTile.transform.SetParent (newRoom.transform);
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof (Controller2D))]
+public class Player : MonoBehaviour {
+
+	[Header("Mouvement")]
+	public float moveSpeed = 6;
+	public float accelarationTimeInAir = 0.2f;
+	public float accelarationTimeGrounded = 0.1f;
+
+	[Header("Jumping")]
+	public float jumpHeight = 4;
+	public float timeToJumpApex = .4f;
+	public float doubleJumpHeightModifier = 1;
+
+	float jumpVelocity;
+	float gravity;
+	Vector3 velocity;
+	float velocityXSmoothing;
+	bool canDoubleJump;
+
+	Controller2D controller;
+
+	void Start () {
+		controller = GetComponent<Controller2D> ();
+
+		gravity = -(2 * jumpHeight / Mathf.Pow (timeToJumpApex, 2));
+		jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+	}
+
+	void Update () {
+
+		if (controller.collisions.above || controller.collisions.below) {
+			velocity.y = 0;
+		}
+
+		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+
+		if (Input.GetMouseButtonDown (1) && controller.collisions.below) {
+			Jump (false);
+		}
+
+		if (!controller.collisions.below && canDoubleJump && Input.GetMouseButtonDown (1)) {
+			Jump (true);
+		}
+
+		float targetVelocityX = input.x * moveSpeed;
+		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelarationTimeGrounded : accelarationTimeInAir);
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move (velocity * Time.deltaTime);
+	}
+
+	void Jump (bool isDoubleJump) {
+		if (!isDoubleJump) {
+			velocity.y = jumpVelocity;
+			canDoubleJump = true;
+		} else {
+			velocity.y = jumpVelocity * doubleJumpHeightModifier;
+			canDoubleJump = false;
+		}
+	}
+
+}
